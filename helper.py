fb0eb3d9a15c46cb22045347bf6dd9833009b945
PyMyCode <pymycode@protonmail.com>
2022-07-15 13:33:29 +0200

    adding env file

--- helper.py
+++ helper.py
@@ -11,7 +11,6 @@ iex_api_key = os.getenv('IEX_API_KEY')
 def get_latest_updates(*symbols):
     for i in symbols:
         ticker = i
-        iex_api_key = 'pk_853c001b291b46c7879b9b80e848ebef'
         
         #creating the API reuqest
         api_url = f'https://cloud.iexapis.com/stable/stock/{ticker}/quote?token={iex_api_key}'
